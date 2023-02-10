02a6c350bff3c59d98676412e13fc78fed021377
momotvit <110504519+momotvit@users.noreply.github.com>
2023-02-09 20:07:21 -0800

    first task completed

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,19 @@
 
 
-const categoriesItemEl = document.querySelectorAll('.item');
-console.log(`Number of categories:`, categoriesItemEl.length);
+// Посчитает и выведет в консоль количество категорий в ul#categories,
+//     то есть элементов li.item.
+// Для каждого элемента li.item в списке ul#categories,
+//     найдет и выведет в консоль текст заголовка
+// элемента(тега < h2 >) и количество элементов
+// в категории(всех вложенных в него < li >).
 
-categoriesItemEl.forEach(item => {
-    const itemHeading = item.firstElementChild.textContent;
-    const itemQuantity = item.querySelectorAll('li').length;
 
-    console.log(`Category:`,itemHeading);
-    console.log(`Elements:`, itemQuantity);
-    return;
-});
 
+const categoriesEl = document.querySelector('#categories');
+console.log(`Number of categories:`, categoriesEl.children.length);
+
+for (const categoryEl of categoriesEl.children) {
+    console.log(`Category:`, categoryEl.firstElementChild.textContent);
+    const categoryUlNodeEL = categoryEl.querySelectorAll('li');
+    console.log(`Elements:`, categoryUlNodeEL.length);
+}
