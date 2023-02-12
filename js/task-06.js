309e24e4d99db0a7741933f37227d6520e808304
momotvit <110504519+momotvit@users.noreply.github.com>
2023-02-11 17:14:28 -0800

    task #6 is completed

--- js/task-06.js
+++ js/task-06.js
@@ -1,32 +1,24 @@
-// Напиши скрипт, который при потере фокуса
-// на инпуте(событие blur), проверяет его содержимое на
-// правильное количество введённых символов.
 
-// <input
-//   type="text"
-//   id="validation-input"
-//   data-length="6"
-//   placeholder="Please enter 6 symbols"
-// />
-
-// Сколько символов должно быть в инпуте, указывается в его атрибуте data-length.
-// Если введено подходящее количество символов, то border инпута
-// становится зелёным, если неправильное - красным.
-// Для добавления стилей, используй CSS - классы valid и invalid,
-//     которые мы уже добавили в исходные файлы задания.
-
-// #validation-input {
-//   border: 3px solid #bdbdbd;
-// }
-
-// #validation-input.valid {
-//   border-color: #4caf50;
-// }
-
-// #validation-input.invalid {
-//   border-color: #f44336;
-// }
+const inputEl = document.querySelector('#validation-input');
+const dataLength = Number(inputEl.dataset.length);
 
 
-const input = document.querySelector('#validation-input');
-console.log(input);
+function borderColorChangeOnBlur() {
+    if (inputEl.value.length === dataLength) {
+        inputEl.classList.add("valid");
+        inputEl.classList.remove("invalid");
+    
+    } else {
+        inputEl.classList.add("invalid");
+        inputEl.classList.remove("valid");
+        
+    }
+}
+
+
+inputEl.addEventListener("blur", borderColorChangeOnBlur);
+        
+
+
+
+
